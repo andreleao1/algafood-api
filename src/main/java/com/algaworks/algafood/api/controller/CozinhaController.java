def497fccad74a430a418355487969ffefe369c4
andreleao1 <andre.gustavo.ls@hotmail.com>
2021-06-22 22:30:15 -0300

    Implementação do serviço REST para a entidade Cozinha

---
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,68 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CozinhaService;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaService cozinhaService;
+
+	@GetMapping
+	public ResponseEntity<List<Cozinha>> listar() {
+		return ResponseEntity.ok(this.cozinhaService.listar());
+	}
+
+	@GetMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable long cozinhaId) {
+		Cozinha cozinhaPesquisada = this.cozinhaService.buscar(cozinhaId);
+
+		if (Objects.nonNull(cozinhaPesquisada)) {
+			return ResponseEntity.ok(cozinhaPesquisada);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Cozinha> salvar(@RequestBody Cozinha cozinha) {
+		this.cozinhaService.salvar(cozinha);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaPesquisada = this.cozinhaService.buscar(cozinhaId);
+
+		if (Objects.nonNull(cozinhaPesquisada)) {
+			BeanUtils.copyProperties(cozinha, cozinhaPesquisada, "id");
+			this.cozinhaService.salvar(cozinhaPesquisada);
+			return ResponseEntity.ok(cozinhaPesquisada);
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Void> remover(@PathVariable long cozinhaId) {
+		this.cozinhaService.excluir(cozinhaId);
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/main/java/com/algaworks/algafood/domain/service/CozinhaService.java
@@ -0,0 +1,48 @@
+package com.algaworks.algafood.domain.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+@Service
+public class CozinhaService {
+
+	private static final String MENSAGEM_ENTIDADE_NAO_ENCONTRADA = "Não foi possível encontrar uma cozinha com id %d";
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public List<Cozinha> listar() {
+		return this.cozinhaRepository.findAll();
+	}
+
+	public Cozinha buscar(long cozinhaId) {
+		Cozinha cozinha = this.cozinhaRepository.findById(cozinhaId).orElseThrow(
+				() -> new EntidadeNaoEncontradaException(String.format(MENSAGEM_ENTIDADE_NAO_ENCONTRADA, cozinhaId)));
+		return cozinha;
+	}
+
+	public Cozinha salvar(Cozinha cozinha) {
+		return this.cozinhaRepository.save(cozinha);
+	}
+
+	public void excluir(long cozinhaId) {
+		if (this.cozinhaRepository.existsById(cozinhaId)) {
+			try {
+				this.cozinhaRepository.deleteById(cozinhaId);
+			} catch (DataIntegrityViolationException e) {
+				throw new EntidadeEmUsoException(
+						(String.format("Cozinha de código %d não pode ser removida, pois está em uso.", cozinhaId)));
+			}
+		} else {
+			throw new EntidadeNaoEncontradaException(String.format(MENSAGEM_ENTIDADE_NAO_ENCONTRADA, cozinhaId));
+		}
+	}
+}
